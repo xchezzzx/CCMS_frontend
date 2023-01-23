e6b308199b42aeb67ef9aefb263180ba4f21c0b2
xchezzzx <50829021+xchezzzx@users.noreply.github.com>
2023-01-23 03:31:30 +0200

    user service

---
+++ SharedLib/Constants/AuthorizationRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedLib.Constants
+{
+	public static class AuthorizationRoles
+	{
+		public static string Administrator = "[\"Administrator\"]";
+		public static string Operator = "[\"Operator\"]";
+		public static string Participant = "[\"Participant\"]";
+	}
+}
